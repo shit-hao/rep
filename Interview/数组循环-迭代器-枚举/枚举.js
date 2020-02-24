e1092d11881a8c3f329592385b077034b074b098
shit-hao <mixihome@outlook.com>
2020-02-24 10:18:05 +0800

    add

--- Interview/数组循环-迭代器-枚举/枚举.js
+++ Interview/数组循环-迭代器-枚举/枚举.js
@@ -1,6 +1,9 @@
 // 在对象里有一个很重要的概念，是否有枚举
 // 一般通过Object.defineProperty 设置枚举属性enumerable
+// Object.defineProperty() 方法会直接在一个对象上定义一个新属性，或者修改一个对象的现有属性， 并返回这个对象。
 // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperties
+// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
+// Object.prototype.propertyIsEnumerable() 方法返回一个布尔值，表示指定的属性是否可枚举。
 // e.g.
 let obj = {b:50,a:30}
 Object.defineProperties(obj,{
@@ -8,6 +11,10 @@ Object.defineProperties(obj,{
         enumerable:true,
         value:30
     }
+})
+Object.defineProperty(obj,'c',{
+        enumerable:true,
+        value:30
 })
 for (const key in obj) {
     if (obj.hasOwnProperty(key)) {
--- ms/yk/3.js
+++ ms/yk/3.js
@@ -1,9 +1,12 @@
 // 手动实现一个深拷贝
 function deepClone(obj){
     console.log(obj)
-    // for (const key of obj) {
-    //     console.log(key)
-    // }
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            const element = obj[key];
+            console.log(element)
+        }
+    }
 }
 let o = {
     a: 3,
