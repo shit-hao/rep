e5c627df0a34b2fd7c8e40404d1c4a9d948847a9
shit-hao <mixihome@outlook.com>
2022-07-10 18:15:31 +0800

    add

--- zzzLuxroid/vscodelc/121.买卖股票的最佳时机.js
+++ zzzLuxroid/vscodelc/121.买卖股票的最佳时机.js
@@ -10,20 +10,18 @@
  * @return {number}
  */
 const maxProfit = prices => {
-  const len = prices.length;
-  // 创建dp数组
-  const dp = new Array(len).fill([0, 0]);
-  // dp数组初始化
-  dp[0] = [-prices[0], 0];
-  for (let i = 1; i < len; i++) {
-    // 更新dp[i]
-    dp[i] = [
-      Math.max(dp[i - 1][0], -prices[i]),
-      Math.max(dp[i - 1][1], prices[i] + dp[i - 1][0]),
-    ];
+  let n = prices.length;
+  let dp = Array.from(new Array(n), () => new Array(2));
+  dp[0][0] = 0; //第0天不持有
+  dp[0][1] = -prices[0]; //第0天持有
+  for (let i = 1; i < n; i++) {
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+    dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
   }
-  return dp[len - 1][1];
+  console.log('dp')
+  console.log(dp)
+  return dp[n - 1][0];
 };
-maxProfit([7,1,5,3,6,4])
+maxProfit([7, 1, 5, 3, 6, 4])
 // @lc code=end
 
